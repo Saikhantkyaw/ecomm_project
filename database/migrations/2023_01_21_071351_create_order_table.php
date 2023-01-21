2877ebe81c4b600b2218c8f8d058327eb17b256b
Saikhantkyaw <saikhantkyaw1551@gmail.com>
2023-01-20 23:55:48 -0800

    first_commit

---
+++ database/migrations/2023_01_21_071351_create_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice');
+            $table->integer('customer_id');
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->integer('total');
+            $table->string('address');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+};
